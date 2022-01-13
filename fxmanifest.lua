fb2e5101ef5c84c5c0f9aea0ba900af930876a2c
Ivan_44 <74063070+ivanprogamer43@users.noreply.github.com>
2022-01-13 13:25:52 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'adamant'
+game 'gta5'
+
+this_is_a_map 'yes'
+
+author 'Ivan_44'
+description 'Script de farmeo y venta de lychees'
+version '0.1'
+
+
+client_script {
+	'client/client.lua',
+	'config.lua'
+} 
+
+server_script {
+	'server/server.lua',
+	'config.lua'
+}
